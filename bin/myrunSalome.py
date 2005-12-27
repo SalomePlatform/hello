901e4ed982b4f05c891d2935af942ce0a1b06107
jfa <jfa@opencascade.com>
2005-12-27 14:59:01 +0000

    Join modifications from branch BR_3_1_0deb

---
+++ bin/myrunSalome.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+def test(clt):
+   """
+        Test function that creates an instance of HELLO component
+        usage : hello=test(clt)
+   """
+   # create an LifeCycleCORBA instance
+   import LifeCycleCORBA
+   lcc = LifeCycleCORBA.LifeCycleCORBA(clt.orb)
+   import HELLO_ORB
+   hello = lcc.FindOrLoadComponent("FactoryServer", "HELLO")
+   return hello
+
+#
+
+if __name__ == "__main__":
+   import user
+   from runSalome import *
+   clt,args = main()
+   
+   #
+   #  Impression arborescence Naming Service
+   #
+   
+   if clt != None:
+     print
+     print " --- registered objects tree in Naming Service ---"
+     clt.showNS()
+     session=clt.waitNS("/Kernel/Session")
+     catalog=clt.waitNS("/Kernel/ModulCatalog")
+     import socket
+     container =  clt.waitNS("/Containers/" + socket.gethostname().split('.')[0] + "/FactoryServer")
