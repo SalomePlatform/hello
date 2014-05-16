492b2c98d4314ebbf685c2d43311a80ae2376232
Christophe Bourcier <christophe.bourcier@cea.fr>
2014-05-16 15:11:28 +0200

    Update translation files from Crowdin

--- src/HELLOGUI/HELLO_msg_ja.ts
+++ src/HELLOGUI/HELLO_msg_ja.ts
@@ -1,115 +1,115 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS>
+<TS version="2.0" language="ja" sourcelanguage="en">
   <context>
     <name>HELLOGUI</name>
     <message>
       <source>MEN_FILE</source>
-      <translation>ファイル(&amp;F)</translation>
+      <translation>MEN_FILE</translation>
     </message>
     <message>
       <source>MEN_FILE_HELLO</source>
-      <translation>こんにちは</translation>
+      <translation>MEN_FILE_HELLO</translation>
     </message>
     <message>
       <source>MEN_HELLO</source>
-      <translation>こんにちは</translation>
+      <translation>MEN_HELLO</translation>
     </message>
     <message>
       <source>MEN_OP_HELLO</source>
-      <translation>こんにちは</translation>
+      <translation>MEN_OP_HELLO</translation>
     </message>
     <message>
       <source>MEN_OP_GOODBYE</source>
-      <translation>さようなら</translation>
+      <translation>MEN_OP_GOODBYE</translation>
     </message>
     <message>
       <source>MEN_OP_TESTME</source>
-      <translation>私にください。</translation>
+      <translation>MEN_OP_TESTME</translation>
     </message>
     <message>
       <source>STS_OP_HELLO</source>
-      <translation>サロメを歓迎します。</translation>
+      <translation>STS_OP_HELLO</translation>
     </message>
     <message>
       <source>STS_OP_GOODBYE</source>
-      <translation>サロメにさよならを言う</translation>
+      <translation>STS_OP_GOODBYE</translation>
     </message>
     <message>
       <source>STS_OP_TESTME</source>
-      <translation>カスタム メニュー アクションのテスト</translation>
+      <translation>STS_OP_TESTME</translation>
     </message>
     <message>
       <source>TLT_OP_HELLO</source>
-      <translation>Salut à 遠い ！</translation>
+      <translation>TLT_OP_HELLO</translation>
     </message>
     <message>
       <source>TLT_OP_GOODBYE</source>
-      <translation>さようなら！</translation>
+      <translation>TLT_OP_GOODBYE</translation>
     </message>
     <message>
       <source>TLT_OP_TESTME</source>
-      <translation>私にください。</translation>
+      <translation>TLT_OP_TESTME</translation>
     </message>
     <message>
       <source>TOOL_TEST</source>
-      <translation>テスト</translation>
+      <translation>TOOL_TEST</translation>
     </message>
     <message>
       <source>TOOL_HELLO</source>
-      <translation>こんにちは</translation>
+      <translation>TOOL_HELLO</translation>
     </message>
     <message>
       <source>BUT_OK</source>
-      <translation>わかりました</translation>
+      <translation>BUT_OK</translation>
     </message>
     <message>
       <source>INF_TESTME_TITLE</source>
-      <translation>こんにちは: 私にください。</translation>
+      <translation>INF_TESTME_TITLE</translation>
     </message>
     <message>
       <source>INF_TESTME_MSG</source>
-      <translation>カスタム メニュー アクションのテストのみです。</translation>
+      <translation>INF_TESTME_MSG</translation>
     </message>
     <message>
       <source>QUE_ENTER_NAME</source>
-      <translation>あなたの名前を入力します。</translation>
+      <translation>QUE_ENTER_NAME</translation>
     </message>
     <message>
       <source>QUE_HELLO_TITLE</source>
-      <translation>サロメを歓迎します。</translation>
+      <translation>QUE_HELLO_TITLE</translation>
     </message>
     <message>
       <source>INF_HELLO_TITLE</source>
-      <translation>こんにちは</translation>
+      <translation>INF_HELLO_TITLE</translation>
     </message>
     <message>
       <source>INF_HELLO_MSG</source>
-      <translation>救済 %1 ！</translation>
+      <translation>INF_HELLO_MSG</translation>
     </message>
     <message>
       <source>ERR_HELLO_ALREADY_MET</source>
-      <translation>%1 はすでに知っている ！</translation>
+      <translation>ERR_HELLO_ALREADY_MET</translation>
     </message>
     <message>
       <source>ERR_ERROR</source>
-      <translation>不明なエラー</translation>
+      <translation>ERR_ERROR</translation>
     </message>
     <message>
       <source>QUE_GOODBYE_TITLE</source>
-      <translation>サロメにさよならを言う</translation>
+      <translation>QUE_GOODBYE_TITLE</translation>
     </message>
     <message>
       <source>INF_GOODBYE_TITLE</source>
-      <translation>さようなら</translation>
+      <translation>INF_GOODBYE_TITLE</translation>
     </message>
     <message>
       <source>INF_GOODBYE_MSG</source>
-      <translation>%1 さようなら ！</translation>
+      <translation>INF_GOODBYE_MSG</translation>
     </message>
     <message>
       <source>ERR_GOODBYE_DID_NOT_MEET</source>
-      <translation>%1 はまだ出会ってません ！</translation>
+      <translation>ERR_GOODBYE_DID_NOT_MEET</translation>
     </message>
   </context>
 </TS>
